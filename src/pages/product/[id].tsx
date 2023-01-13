12915c3793e3bef731e533e8773207a2c0add17f
Juliana Daroz <julianadaroz@gmail.com>
2023-01-13 18:09:20 -0300

    Installed axios and made checkout handler

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,6 +1,8 @@
+import axios from "axios"
 import { GetStaticPaths, GetStaticProps } from "next"
 import Image from "next/image"
 import { useRouter } from "next/router"
+import { useState } from "react"
 import Stripe from "stripe"
 
 import { ImageContainer, ProductContainer, ProductDetails } from "../../../styles/pages/product"
@@ -13,14 +15,35 @@ interface ProductProps {
     imageUrl: string,
     price: string,
     description: string,
+    defaultPriceId: string,
   }
 }
 
 export default function Product({ product }: ProductProps){
+  const [isCreatingCheckoutSession, setIsCreatingCheckoutSession] = useState(false)
   const { isFallback } = useRouter()
 
   if (isFallback) {
     return  <p>Loading...</p>
+  }
+
+  async function handleBuyProduct() {
+    try {
+      setIsCreatingCheckoutSession(true)
+
+      const response = await axios.post("/api/checkout", {
+        priceId: product.defaultPriceId,
+      })
+
+      const { checkoutUrl } = response.data
+
+      window.location.href = checkoutUrl
+    } catch (err) {
+      // conectar com uma ferramenta de observabilidade (Datadog/ Sentry)
+      setIsCreatingCheckoutSession(false)
+
+      alert("Falha ao redirecionar ao checkout!")
+    }
   }
 
   return (
@@ -35,7 +58,7 @@ export default function Product({ product }: ProductProps){
 
         <p>{product.description}</p>
 
-        <button>
+        <button disabled={isCreatingCheckoutSession} onClick={() => {handleBuyProduct()}}>
           Comprar agora
         </button>
       </ProductDetails>
@@ -73,7 +96,8 @@ export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ para
           style: "currency",
           currency: "BRL",
         }).format(price.unit_amount! / 100),   
-        description: product.description,     
+        description: product.description,
+        defaultPriceId: price.id     
       }
     },
     revalidate: 60 * 60 * 1 // 1 hour
